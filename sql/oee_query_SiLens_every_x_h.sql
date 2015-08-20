d8248edbca5c8a55427e074b0e784196f7976bb0
Aldo Mendez <aldomendez86@gmail.com>
2015-08-20 16:00:40 -0500

    Ya se le hicieron los cambios que se revisaron en la junta
    - Considerar todo para lo que se estuvo utilizando la maquina en
      availability (ensamble, pruebas de estandares, piezas de calificacion)
    - Considerar solamente material de produccion para performance
    - Considerar solamente material de produccion para el Yield es por
      extension del punto anterior, ya que es la base de el calculo de yield

--- sql/oee_query_SiLens_every_x_h.sql
+++ sql/oee_query_SiLens_every_x_h.sql
@@ -3,13 +3,12 @@ to_date(':final','yyyy-mm-dd hh24:mi') s_end_dt, a.* , (availability * performan
 select bu_id, depto, product, process, machine, round((to_date(':final','yyyy-mm-dd hh24:mi') - to_date(':inicio','yyyy-mm-dd hh24:mi'))*24*60,0) 
 sample_span_time, round(sum(cycle_time/60),0) total_production_time,
   count(serial_num) build_qty,
-  round(sum(cycle_time/60)/count(serial_num),2) avg_ct,
+  round(sum(case when step_name = 'TOSA SUBASSEM3 (SUBASSEM2, SI LENS)' then cycle_time/60 else 0 end)/count(serial_num),2) avg_ct,
   sum(cycle_time/60)/round((to_date(':final','yyyy-mm-dd hh24:mi') - to_date(':inicio','yyyy-mm-dd hh24:mi'))*24*60,0) availability,
-  (20/(sum(cycle_time/60) /count(serial_num))) Performance,
-  sum(case when PASS_FAIL = 'P' then 1 else 0 end)/count(serial_num) Yield
+  (oee_machine_catalog.IDEAL_CYCLE_TIME/(sum(case when step_name = 'TOSA SUBASSEM3 (SUBASSEM2, SI LENS)' then cycle_time/60 else 0 end) /count(serial_num))) Performance,
+  sum(case when PASS_FAIL = 'P' and step_name = 'TOSA SUBASSEM3 (SUBASSEM2, SI LENS)' then 1 else 0 end)/count(serial_num) Yield
 from phase2.lr4_shim_assembly@mxoptix left join apogee.oee_machine_catalog on system_id=machine 
 where process_date between to_date(':inicio','yyyy-mm-dd hh24:mi') and to_date(':final','yyyy-mm-dd hh24:mi') and 
-  system_id in ('CYBOND38','CYBOND57','CYBOND63','CYBOND60','CYBOND55','CYBOND56','CYBOND14','CYBOND3','CYBOND59','CYBOND58') 
-  and step_name = 'TOSA SUBASSEM3 (SUBASSEM2, SI LENS)'
-group by bu_id, depto, product, process, machine
+  system_id in ('CYBOND38','CYBOND57','CYBOND63','CYBOND60','CYBOND55','CYBOND56','CYBOND14','CYBOND3','CYBOND59','CYBOND58')
+group by bu_id, depto, product, process, machine, oee_machine_catalog.IDEAL_CYCLE_TIME
 )a
