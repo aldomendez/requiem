79272b359ee826f0243262809e2113f06a237ebe
Aldo Mendez <aldomendez86@gmail.com>
2015-09-11 14:55:20 -0500

    Ya toma ideal_cycle_time de la tabla (no hardcoded)

--- sql/oee_query_Engines_Welder.sql
+++ sql/oee_query_Engines_Welder.sql
@@ -5,7 +5,7 @@ select bu_id bu,depto,product,process,system_id,round((to_date(':final','yyyy-mm
     count(serial_num) build_qty,
     round(sum(cycle_time)/count(serial_num),2) avg_ct,
     sum(cycle_time)/round((to_date(':final','yyyy-mm-dd hh24:mi') - to_date(':inicio','yyyy-mm-dd hh24:mi'))*24*60,0) availability,
-    6/(sum(cycle_time) /count(serial_num)) Performance,
+    ideal_cycle_time/(sum(cycle_time) /count(serial_num)) Performance,
     sum(case when PASS_FAIL = 'PASS' then 1 else 0 end)/count(serial_num) Yield
 from (select welder_id system_id,device serial_num,device_fm pass_fail,end_dt process_date, (end_dt- test_dt)*24*60 cycle_time,state from 
 (dare_pkg.fiber_weld_prod@prodmx)a where test_dt between to_date(':inicio','yyyy-mm-dd hh24:mi') 
@@ -14,5 +14,5 @@ where process_date between
   to_date(':inicio','yyyy-mm-dd hh24:mi') and 
   to_date(':final','yyyy-mm-dd hh24:mi') and
   system_id in ('AOI6','AOI21','AOI5','AOI1','AOI11')
-group by system_id, bu_id, depto, product, process,machine 
+group by system_id, bu_id, depto, product, process,machine, ideal_cycle_time
 )a
